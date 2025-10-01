1d758de95ab931eba0d1ec7c7aebae8096087cdf
David Enke <post@davidenke.de>
2025-10-01 15:33:15 +0200

    test: adopt wording of other tests

--- src/rules/consistent-spacing-between-blocks.test.ts
+++ src/rules/consistent-spacing-between-blocks.test.ts
@@ -5,19 +5,19 @@ runRuleTester('consistent-spacing-between-blocks', rule, {
   invalid: [
     {
       code: javascript`
-        test.beforeEach('Test 1', () => {});
-        test('Test 2', async () => {
-            await test.step('Step 1', () => {});
+        test.beforeEach('should pass', () => {});
+        test('should fail', async () => {
+            await test.step('should pass', () => {});
             // a comment
-            test.step('Step 2', () => {});
-            test.step('Step 3', () => {});
-            const foo = await test.step('Step 4', () => {});
-            foo = await test.step('Step 5', () => {});
+            test.step('should fail', () => {});
+            test.step('should fail', () => {});
+            const foo = await test.step('should fail', () => {});
+            foo = await test.step('should fail', () => {});
         });
         /**
          * another comment
          */
-        test('Test 6', () => {});
+        test('should fail', () => {});
       `,
       errors: [
         { line: 2, messageId: 'missingWhitespace' },
@@ -29,34 +29,34 @@ runRuleTester('consistent-spacing-between-blocks', rule, {
       ],
       name: 'missing blank lines before test blocks',
       output: javascript`
-        test.beforeEach('Test 1', () => {});
+        test.beforeEach('should pass', () => {});
 
-        test('Test 2', async () => {
-            await test.step('Step 1', () => {});
+        test('should fail', async () => {
+            await test.step('should pass', () => {});
 
             // a comment
-            test.step('Step 2', () => {});
+            test.step('should fail', () => {});
 
-            test.step('Step 3', () => {});
+            test.step('should fail', () => {});
 
-            const foo = await test.step('Step 4', () => {});
+            const foo = await test.step('should fail', () => {});
 
-            foo = await test.step('Step 5', () => {});
+            foo = await test.step('should fail', () => {});
         });
 
         /**
          * another comment
          */
-        test('Test 6', () => {});
+        test('should fail', () => {});
       `,
     },
   ],
   valid: [
     {
       code: javascript`
-        test('Test 1', () => {});
+        test('should pass', () => {});
 
-        test('Test 2', () => {});
+        test('should pass', () => {});
       `,
       name: 'blank line between simple test blocks',
     },
@@ -64,40 +64,40 @@ runRuleTester('consistent-spacing-between-blocks', rule, {
       code: javascript`
         test.beforeEach(() => {});
 
-        test.skip('Test 2', () => {});
+        test.skip('should pass', () => {});
       `,
       name: 'blank line between test modifiers',
     },
     {
       code: javascript`
-        test('Test', async () => {
-            await test.step('Step 1', () => {});
+        test('should pass', async () => {
+            await test.step('should pass', () => {});
 
-            await test.step('Step 2', () => {});
+            await test.step('should pass', () => {});
         });
       `,
       name: 'blank line between nested steps in async test',
     },
     {
       code: javascript`
-        test('Test', async () => {
-            await test.step('Step 1', () => {});
+        test('should pass', async () => {
+            await test.step('should pass', () => {});
 
             // some comment
-            await test.step('Step 2', () => {});
+            await test.step('should pass', () => {});
         });
       `,
       name: 'nested steps with a line comment in between',
     },
     {
       code: javascript`
-        test('Test', async () => {
-            await test.step('Step 1', () => {});
+        test('should pass', async () => {
+            await test.step('should pass', () => {});
 
             /**
              * another comment
              */
-            await test.step('Step 2', () => {});
+            await test.step('should pass', () => {});
         });
       `,
       name: 'nested steps with a block comment in between',
@@ -105,9 +105,9 @@ runRuleTester('consistent-spacing-between-blocks', rule, {
     {
       code: javascript`
         test('assign', async () => {
-            let foo = await test.step('Step 1', () => {});
+            let foo = await test.step('should pass', () => {});
 
-            foo = await test.step('Step 2', () => {});
+            foo = await test.step('should pass', () => {});
         });
       `,
       name: 'assignments initialized by test.step',
@@ -115,9 +115,9 @@ runRuleTester('consistent-spacing-between-blocks', rule, {
     {
       code: javascript`
         test('assign', async () => {
-            let { foo } = await test.step('Step 1', () => {});
+            let { foo } = await test.step('should pass', () => {});
 
-            ({ foo } = await test.step('Step 2', () => {}));
+            ({ foo } = await test.step('should pass', () => {}));
         });
       `,
       name: 'destructuring assignments initialized by test.step',
