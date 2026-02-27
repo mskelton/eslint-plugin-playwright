7e16bd565cfccd365a6a8f1f7f6fe29a1c868036
Mark Skelton <mdskelton99@gmail.com>
2026-02-27 12:04:36 -0600

    docs: Fix readme for some rules

--- src/rules/rules.test.ts
+++ src/rules/rules.test.ts
@@ -1,6 +1,5 @@
-import assert from 'node:assert'
 import fs from 'node:fs/promises'
-import { test } from 'vitest'
+import { expect, test } from 'vitest'
 
 const { flatConfig } = await import('../../src/plugin.ts')
 
@@ -13,15 +12,32 @@ test('exports all rules', async () => {
     .map((file) => file.replace('.ts', ''))
     .sort()
 
-  assert.deepEqual(ruleKeys, fileKeys)
+  expect(ruleKeys).toEqual(fileKeys)
 })
 
-test('has all rules in the README', async () => {
+function getReadmeRow(readme: string, rule: string) {
+  return readme.split('\n').find((line) => line.includes(`[${rule}]`))
+}
+
+test('rules properly documented in README', async () => {
   const readme = await fs.readFile(new URL('../../README.md', import.meta.url), 'utf-8')
-
   const { rules } = flatConfig.plugins.playwright
+  const recommendedRules = new Set(
+    Object.keys(flatConfig.rules).map((key) => key.replace('playwright/', '')),
+  )
 
-  for (const rule of Object.keys(rules)) {
-    assert.ok(readme.includes(`[${rule}]`))
+  for (const [name, rule] of Object.entries(rules)) {
+    const row = getReadmeRow(readme, name)
+    expect(row, `Rule ${name} not found in README`).toBeDefined()
+
+    const columns = row!.split('|').map((col) => col.trim())
+
+    expect(columns[3] === '✅', `Rule ${name} recommended mismatch`).toBe(
+      recommendedRules.has(name),
+    )
+    expect(columns[4] === '🔧', `Rule ${name} fixable mismatch`).toBe(!!rule.meta?.fixable)
+    expect(columns[5] === '💡', `Rule ${name} suggestions mismatch`).toBe(
+      !!rule.meta?.hasSuggestions,
+    )
   }
 })
